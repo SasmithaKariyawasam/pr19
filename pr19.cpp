60a1bb96d6b9933b48294651f62305187f1c25fc
Sasmitha Kariyawasam <Sasamavikariyawasam12@gmail.com>
2024-11-17 20:58:53 +0530

    Add files via upload

---
+++ pr19.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cout<<"Enter the marks: ";
+    cin>>marks;
+
+    if(marks>=40)
+    {
+        cout<<"Pass";
+    }else
+    {
+        cout<<"Fail";
+    }
+    return 0;
+
+}
+
+
